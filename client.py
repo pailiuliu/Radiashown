a0420a4b209aa4d5b81c10568ecd4c93b2824879
kmonkhouse <monkhousek@gmail.com>
2020-02-01 20:28:07 -0500

    Succesfully parses Az El and 8 Detector Cps values

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+#!python2
 # An example script to connect to Google using socket 
 # programming in Python 
 import socket # for socket 
@@ -27,6 +28,19 @@ s.connect((host_ip, port))
 print "the socket has successfully connected"
 az = []
 el = []
+detCps1 = []
+detCps2 = []
+detCps3 = []
+detCps4 = []
+detCps5 = []
+detCps6 = []
+detCps7 = []
+detCps8 = []
+
+detCps = [None] * 8
+
+detectors = [detCps1, detCps2, detCps3, detCps4, detCps5, detCps6, detCps7, detCps8]
+
 i = 1
 
 while (1):
@@ -36,7 +50,23 @@ while (1):
             az.append(ord(msg))
         if 46 <= i <= 53:
             el.append(ord(msg))
-            
+        if 54 <= i <= 61:
+            detCps1.append(ord(msg))
+        if 62 <= i <= 69:
+            detCps2.append(ord(msg))
+        if 70 <= i <= 77:
+            detCps3.append(ord(msg))
+        if 78 <= i <= 85:
+            detCps4.append(ord(msg))
+        if 86 <= i <= 93:
+            detCps5.append(ord(msg))
+        if 94 <= i <= 101:
+            detCps6.append(ord(msg))
+        if 102 <= i <= 109:
+            detCps7.append(ord(msg))
+        if 110 <= i <= 117:
+            detCps8.append(ord(msg))
+                   
         i += 1
     else:
         azUni = struct.pack('BBBBBBBB', *az)
@@ -47,8 +77,24 @@ while (1):
 
         print "Azimuth = %f\n" % azimuth
         print "Elevation = %f\n" % elevation
+
+        for d in range(0,8):
+            thisCps = detectors[d]
+            thisCpsUni = struct.pack('BBBBBBBB', *thisCps)
+            detCps[d] = struct.unpack('<d', thisCpsUni)[0]
+            print "Detector %d Cps = %f\n" % (d, detCps[d])
+            
         az = []
         el = []
+        detCps1 = []
+        detCps2 = []
+        detCps3 = []
+        detCps4 = []
+        detCps5 = []
+        detCps6 = []
+        detCps7 = []
+        detCps8 = []
+
         i = 1
 
     
